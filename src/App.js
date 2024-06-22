5c6bb9e64005d085effa28f7bb9afe94556530e0
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2024-06-22 22:00:32 +0530

    Added decrement counter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   const incrementCount = useCallback(() => setCount(count + 1), [count]); 
+  const decrementCount = useCallback(() => setCount(count - 1), [count]); 
 
   return (
     <div className="App">
@@ -15,21 +16,23 @@ function App() {
         onChange={(e) => setInput(e.target.value)} 
       />
       <button onClick={incrementCount}>Increment Counter</button>
+      <button onClick={decrementCount}>Decrement Counter</button>
       <h3>Input text: {input}</h3>
       <h3>Count: {count}</h3>
       <hr />
-      <ChildComponent count={count} onClick={incrementCount} />
+      <ChildComponent count={count} onIncrement={incrementCount} onDecrement={decrementCount} />
     </div>
   );
 }
 
-const ChildComponent = React.memo(function({ count, onClick }) {
+const ChildComponent = React.memo(function({ count, onIncrement, onDecrement }) {
   console.log("Child component is rendering");
   return (
     <div>
       <h3>Child Component</h3>
       <h3>Count: {count}</h3>
-      <button onClick={onClick}>Increment Counter</button>
+      <button onClick={onIncrement}>Increment Counter</button>
+      <button onClick={onDecrement}>Decrement Counter</button>
     </div>
   );
 });
